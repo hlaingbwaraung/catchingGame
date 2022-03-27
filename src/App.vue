5d37c7da98cd466d872e67cb5652404a70e24dc4
hbanihon <mizbwar@gmail.com>
2022-03-28 01:42:01 +0900

    1st game

--- src/App.vue
+++ src/App.vue
@@ -2,37 +2,42 @@
   <h1>How fast can u catch me?</h1>
   <button @click="start" :disabled="isPlaying">Play</button>
   <div v-if="isPlaying">
-     <Block :delay="delay" @endGame="endGame"/>
+    <Block :delay="delay" @endGame="endGame" />
   </div>
- <p>Result is {{score}}</p>
+  <div v-if="end">
+    <Result :score="score" />
+  </div>
 </template>
 
 <script>
-import Block from "./components/Block.vue"
-
+import Block from "./components/Block.vue";
+import Result from "./components/Result.vue";
 
 export default {
-  name:'App',
-  components:{
-    Block
+  name: "App",
+  components: {
+    Block,
+    Result,
   },
-data(){
-      return{
-        isPlaying:false,
-        delay:null
-      }
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: 0,
+      end: false,
+    };
   },
-  methods:{
-    start(){
-        this.isPlaying=true,
-        this.delay=2000+Math.random()*5000;
+  methods: {
+    start() {
+      (this.isPlaying = true), (this.delay = 2000 + Math.random() * 5000);
     },
-      endGame(score){
-    console.log(score)
-  }
+    endGame(score) {
+      this.score = score;
+      this.isPlaying = false;
+      this.end = true;
+    },
   },
-
-}
+};
 </script>
 
 <style>
--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,44 +1,44 @@
 <template>
-  <div class="Block" v-if="showBlock" @click="stopTimer"> Catch me</div>
+  <div class="Block" v-if="showBlock" @click="stopTimer">Catch me</div>
 </template>
 
 <script>
 export default {
- props:['delay'],
- data(){
-     return{
-         showBlock:false,
-         score:0,
-         timer:null
-     }
- },
- mounted(){
-     setTimeout(() => {
-         this.showBlock = true;
-         this.startTimer()
-     }, this.delay);
- },
- methods:{
-     startTimer(){
-         this.timer = setInterval(() => {
-         this.score+=50;
-     }, 50);
-     },
-     stopTimer(){
-         clearInterval(this.timer);
-         this.$emit("endGame",this.score)
-     }
- }
-}
+  props: ["delay"],
+  data() {
+    return {
+      showBlock: false,
+      score: 0,
+      timer: null,
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.showBlock = true;
+      this.startTimer();
+    }, this.delay);
+  },
+  methods: {
+    startTimer() {
+      this.timer = setInterval(() => {
+        this.score += 50;
+      }, 50);
+    },
+    stopTimer() {
+      clearInterval(this.timer);
+      this.$emit("endGame", this.score);
+    },
+  },
+};
 </script>
 
 <style>
-.Block{
-    background-color: aquamarine;
-    color: black;
-    width: 150px;
-    height: 150px;
-    padding:100px;
-    margin: 120px auto;
+.Block {
+  background-color: aquamarine;
+  color: black;
+  width: 150px;
+  height: 150px;
+  padding: 100px;
+  margin: 120px auto;
 }
 </style>
